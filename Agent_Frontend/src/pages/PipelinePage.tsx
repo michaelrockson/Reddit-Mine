fa1f61f2d39484388a70e3ae9681435aace71443
rocksoncodes <michaelrockszn@gmail.com>
2026-06-11 12:57:23 +0000

    update: update pipeline page

--- Agent_Frontend/src/pages/PipelinePage.tsx
+++ Agent_Frontend/src/pages/PipelinePage.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { AgentPipeline } from "../features/pipeline/index..ts";
-import UserGreeting from "../components/ui/UserGreeting.tsx";
 import PipelineRunHistory from "../features/pipeline/components/PipelineRunHistory.tsx";
 
 export default function PipelinePage(): React.JSX.Element {
   return (
     <section className="li-section-sm li-px-lg li-flex li-flex-col li-h-full">
-      <UserGreeting username={"Michael Rockson"} />
       <AgentPipeline />
       <PipelineRunHistory />
     </section>
