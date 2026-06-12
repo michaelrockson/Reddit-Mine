7bb26d8f202fc03c2171e30f28d46696298ab3e4
rocksoncodes <michaelrockszn@gmail.com>
2026-06-12 09:26:59 +0000

    feat(Component): add report header

---
+++ Agent_Frontend/src/pages/ReportsPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReportsHeader } from "../features/reports/index.ts";
+
+export default function ReportsPage(): React.JSX.Element {
+  return (
+    <section className="li-section-sm li-px-lg li-flex li-flex-col li-gap-lg li-h-full">
+      <ReportsHeader />
+    </section>
+  );
+}
